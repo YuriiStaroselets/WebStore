0cd9405a7ad77377142ebddff03b7b6abfa6bcc8
YuriiStaroselets <yuriistaroselets@gmail.com>
2023-12-17 13:58:13 +0200

    Kuder

--- webshop/settings.py
+++ webshop/settings.py
@@ -91,8 +91,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hello',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # або IP-адреса вашого сервера MySQL
+        'PORT': '3306',       # порт, на якому працює MySQL (зазвичай 3306)
     }
 }
 
